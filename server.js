cea7ab9b703f5bc0334994a7f3e4e02e62c3f732
Or Antebi <or87600@gmail.com>
2025-03-18 16:23:17 +0200

    add initial components, styles, and server routes for the Miss Bug project

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 import express from 'express'
 const app = express()
 
+app.get('/api/bug', (req, res) => {})
+app.get('/api/bug/save', (req, res) => {})
+app.get('/api/bug/:bugId', (req, res) => {})
+app.get('/api/bug/:bugId/remove', (req, res) => {})
+
+
 app.get('/', (req, res) => res.send('Hello there'))
-app.listen(3030, () => console.log('Server ready at port 3030'))
+app.listen(3031, () => console.log('Server ready at port 3031'))
